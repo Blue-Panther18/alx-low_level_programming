537350bc3db1e12169480a10bcc9251d5a5c0b1a
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-09-10 15:29:29 -0700

    edited a file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
  * Return: Always 0 
- *
  * print: some words
  */
 
 int main(void)
 {
-	char dis[] = "\"Programming is like building a multlingual puzzle\n";
+	char dis[100] = "\"Programming is like building a multlingual puzzle\n";
+	
 	puts(dis);	
 	return (0);
 }
