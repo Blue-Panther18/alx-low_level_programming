bd38222829e42d6be061410bba0e7597983dc5fd
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-09-10 15:19:23 -0700

    added a file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ * 
+ * Return: Always 0 
+ *
+ * print: some words
+ */
+
+int main(void)
+{
+	char dis[] = "Programming is like building a multlingual puzzle\n";
+
+	puts(dis);
+	
+	return (0);
+}
