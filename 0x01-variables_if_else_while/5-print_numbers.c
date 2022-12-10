39134bad9895666e180a76b2e36833c36921b047
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-10 22:01:43 +0000

    added a file that print all base 10 single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all base 10 digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	};
+	printf("\n");
+	return (0);
+}
