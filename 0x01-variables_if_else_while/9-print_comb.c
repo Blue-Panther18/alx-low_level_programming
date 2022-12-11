a94c1d3e5d76137e0fd2aeb08a4478fe7100f084
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-11 18:01:57 +0000

    added a new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all possible single digit number combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		if (a == 57)
+		{
+			putchar(a);
+			putchar('$');
+			a++;
+		} else
+		{
+			putchar(a);
+			putchar(',');
+			putchar(' ');
+			a++;
+		};
+	};
+	putchar('\n');
+	return (0);
+}
