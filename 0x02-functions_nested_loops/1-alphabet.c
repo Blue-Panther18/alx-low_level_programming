09b2bc8d3311cd92ef5f8b56b4cd8910e22d4b28
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-14 23:00:15 +0000

    added some new files and edited some files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print all the alphabets in  lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	x= 'a';
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	};
+	_putchar('\n');
+}
