baa1c6f555c9c673c080080526360b041cb5c613
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-15 00:13:35 +0000

    created some files and edited file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 for lowercase character
+ * @c: this is character that will checked
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
+
