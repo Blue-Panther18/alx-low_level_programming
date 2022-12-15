9b0586092b76a8e4358f9732707253303dbc6e3b
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-15 09:06:07 +0000

    created some files and edited file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,14 @@
 
 /**
  * _abs - displays the absolute value of an integer
- *
+ * @a: integer parameter
  * Return: Always 0.
  */
-int _abs(int)
+int _abs(int a)
 {
+	if (a < 0)
+		return (a * -1);
+	else
+		return (a);
+}
 
