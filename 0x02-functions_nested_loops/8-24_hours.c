448028ea7549c031ffad52099830ff3c90c50084
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-16 02:15:35 +0000

    created some files and edited file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - display every minutes of the day
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i < '3'; i++)
+	{
+		for (j = '0'; j < '4'; j++)
+		{
+			for (k = '0'; k < '6'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
+
+
