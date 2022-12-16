0dcfc0cbbb1e03fa40b8c9001849e90f13445ebe
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-16 22:40:20 +0000

    This file display the sum of the multiple of 3 or 5 to the standard I/O

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - display the sum of multiple of 3 or 5 between 0 - 1024
+ */
+void main(void)
+{
+	int a, b, sum;
+
+	a = 1024;
+	sum = 0;
+	for (b = 0; b < a; b++)
+	{
+		if (b % 3 == 0 || b % 5 == 0)
+		{
+			sum = sum + b;
+		};
+	}
+	printf("%d\n", sum);
+}
+
