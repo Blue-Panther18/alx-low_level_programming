b9b3134fd6623157e32f14626191e3fff0d5abf2
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-17 20:24:56 +0000

    The file print the sum of all even series of the fibonacci that less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - display sum of even fibonacci series that doesn't exceed 4,000,000
+ * Return: 0
+ */
+int main(void)
+{
+	long a, b, c, sum;
+
+	a = 1;
+	b = 2;
+	c = 0;
+	sum = 0;
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0)
+			sum += c;
+	}
+	printf("%ld\n", sum);
+}
