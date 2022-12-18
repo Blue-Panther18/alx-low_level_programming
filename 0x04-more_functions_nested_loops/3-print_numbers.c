5b144198396948a50ba55c4dc64ee4b4e68764e8
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-18 15:28:59 +0000

    created a new file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - display integer from 1 - 9
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
