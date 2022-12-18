be3da110e2e14f90943802250a68e690a4ec210c
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-18 21:00:56 +0000

    created a c file and a compiled file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,11 +5,19 @@
  */
 void print_diagonal(int n)
 {
-	int a;
-	
-	for (a = 0; a < n; a++)
-	{
-		_putchar(92);
+	int a, b;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (a = 0; a < n; a++)
+		{
+			if (a > 0)
+			{
+				for (b = 0; b < a; b++)
+					_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
 	}
-	_putchar('\n');
 }
