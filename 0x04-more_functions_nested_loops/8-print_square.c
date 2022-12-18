ef2c7d86eb3fd08fb3a29246f5db9a3b0718f086
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-18 21:18:36 +0000

    created and compiled a file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - display # size times
+ * @size : integer parameter
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+			_putchar('\n');
+		}
+}
