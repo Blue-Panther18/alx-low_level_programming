3610fa5efb7c64a71211f18a7df8a40d3bf17216
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-18 20:06:47 +0000

    created and compiled a file that print - n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - displays a line n times
+ * @n: integer parameter that control the number of lines
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+		_putchar('-');
+	_putchar('\n');
+}
