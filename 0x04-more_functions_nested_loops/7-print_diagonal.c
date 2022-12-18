cd0405cea9e61540137b533e923f5e6a5fec4f23
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-18 20:33:19 +0000

    created a c file and a compiled file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_diagonal - display \ n times
+ * @n: integer parameter determines the number of times \ print
+ */
+void print_diagonal(int n)
+{
+	int a;
+	
+	for (a = 0; a < n; a++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
