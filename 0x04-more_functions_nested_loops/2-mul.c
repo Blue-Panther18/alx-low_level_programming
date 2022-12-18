44583952ec2c68e8d973b6de982468cef561fbe1
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-18 14:36:42 +0000

    created and compiled a file that return the multiple of two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - returns the multiplication of two integer
+ * @a: first parameter
+ * @b: second parameter
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
