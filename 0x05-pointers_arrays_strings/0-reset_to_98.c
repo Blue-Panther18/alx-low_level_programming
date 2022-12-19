ff7911497936cff792cc36678d69560d5cf6bbc4
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-19 13:32:31 +0000

    created and compiled a file that change the value of a variable through pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of the variable
+ *
+ * @n: integer pointer parameter
+ *
+ * Return: Nothinh.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
