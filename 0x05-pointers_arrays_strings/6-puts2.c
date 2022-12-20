41c4c47689f4e59174ff3fba38bf0491d7605e8a
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-20 02:40:37 +0000

    created and compiled a file that print every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ *
+ * @s: string parameter
+ *
+ * Return: Nothing.
+ */
+void puts2(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (*(s + i) % 2 == 0)
+			_putchar(s[i]);
+	}
+	_putchar('\n');
+}
