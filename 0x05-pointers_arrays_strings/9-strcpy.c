3c542a0f973990db6c71aa32e9370830a5f558bc
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-20 04:23:23 +0000

    edited a file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	i = 0;
+	i = -1;
 
 	do {
-		*(dest + i) = src[i];
 		i++;
+		*(dest + i) = src[i];
 	} while (src[i] != '\0');
 
 	return (dest);
