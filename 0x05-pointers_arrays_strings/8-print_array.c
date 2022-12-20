917926bd78f7c842f4fa5e6be549aeb67a08f277
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-20 03:51:14 +0000

    created and compiled a file that print the elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print the elements of an array
+ *
+ * @a: array parameter
+ *
+ * @n: number of the elements in the array
+ *
+ * Return: Nothing.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while(i < n)
+	{
+		printf("%d", a[i]);
+		i++;
+		if (i != n)
+			printf(", ");
+	}
+	putchar('\n');
+}
