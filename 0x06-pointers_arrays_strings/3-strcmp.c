c0ce4e89e754abff06840c9fb9df7b1dd36c2177
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-22 20:58:29 +0000

    created and compiled a C file that compare two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, k;
+	int i;
 
-	k = 0;
-	j = 0;
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] > s2[i])
-			k++;
-		else if (s1[i] < s2[i])
-			j++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	if (k > j)
-		i = 15;
-	else if (k < j)
-		i = -15;
-	else
-		i = 0;
-	return (i);
+	return (0);
 }
