17c0734d96c24ed5c72643fcddb3b0579553135c
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-23 00:03:27 +0000

    added a file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize first letter a word after separators
+ * @str: String parameter
+ *
+ * Return: String.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i - 1] == 32 ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}')
+				str[i] -= 32;
+		}
+	}
+	return (str);
+}
