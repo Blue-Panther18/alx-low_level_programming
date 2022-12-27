75cdd79afde450ef9e8ccf670f9064edffd17472
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2022-12-27 02:41:41 +0000

    created and compiled a file that encode a string and returned the encoded string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encode a string to 1337
+ *
+ * @str: String parameter
+ *
+ * Return: encoded string.
+ */
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet[] = {'O', 'L', '#', 'E', 'A', '#', '#', 'T'};
+
+	while (str[i])
+	{
+		for (j = 0; j < 8; j++)
+			if (str[i] == leet[j] ||
+					str[i] - 32 == leet[j])
+			{
+				str[i] = j + '0';
+			}
+		i++;
+	}
+	return (str);
+}
