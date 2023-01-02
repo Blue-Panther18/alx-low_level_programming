eb81bba5a32ede220c80972b8771ec7d2c6a7113
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2023-01-02 16:53:28 +0000

    renamed the directory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - 
+ *
+ * @s:
+ * @b:
+ * @n:
+ *
+ * Return: 
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains the files Alx tasks pointers, arrays and strings.
