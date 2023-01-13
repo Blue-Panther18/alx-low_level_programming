5b7bc3e228afde2ed9202e6a5dda497b9d2e2bfd
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2023-01-13 20:16:06 +0000

    created and compiled a C function file that returns the length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: ctring parameter
+ *
+ * Return: Integer for length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s)
+	{
+		i = 1 + _strlen_recursion(s + 1);
+	}
+	return (i);
+}
